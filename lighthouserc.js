59ae3460360486e0aa492bf3f6ced0579db36f0f
PB <pbaranski@users.noreply.github.com>
2021-10-15 15:01:17 +0200

    Create lighthouserc.js

---
+++ lighthouserc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  ci: {
+    collect: {
+      numberOfRuns: 1,
+      url: ['https://me-front-perf-test1-lh.herokuapp.com'],
+      settings: {
+        chromeFlags: '--no-sandbox',
+      },
+    },
+    assert: {
+      preset: 'lighthouse:no-pwa',
+      assertions: {
+        'uses-rel-preload': 'off',
+        'uses-rel-preconnect': 'off',
+      },
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+}
